507569a81e6733e3b1ef86793d0a3496b75609a1
Sergey Yatsuk <sergeyy@cqg.com>
2017-03-12 19:18:15 +0200

    GCJ_2015_1A_B

--- GoogleCodeJam/GCJ_2015_1A_B/main.cpp
+++ GoogleCodeJam/GCJ_2015_1A_B/main.cpp
@@ -30,15 +30,79 @@ using namespace tr1;
 typedef long long ll;
 typedef unsigned long long ull;
 
-void solve(int test)
+ll gcd(ll a, ll b)
+{
+   if (b == 0)
+      return a;
+   return gcd(b, a%b);
+}
+
+ll findLCM(vector<int>& vec) {
+   ll ans = vec[0];
+
+   for (ll i = 1; i < vec.size(); i++)
+      ans = (((vec[i] * ans)) /
+      (gcd(vec[i], ans)));
+
+   return ans;
+}
+
+void solve(ll test)
 {
    printf("Case #%d: ", test + 1);
+   ll B, N;
+   cin >> B;
+   cin >> N;
+
+   int minT = 100000;
+   int newMinT = 100000;
+   vector<int> m(B);
+   for (int i = 0; i < B; ++i) {
+      cin >> m[i];
+      minT = min(minT, m[i]);
+   }
+
+   ll processed = 0;
+   ll lcm = findLCM(m);
+   for (int i = 0; i < B; ++i) {
+      processed += lcm / m[i];
+   }
+
+
+   N = N % processed;
+   if (N == 0) {
+      printf("%d\n", B);
+      return;
+   }
+
+   vector<int> q(B);
+
+   int lastI = -1;
+   while (N > 0) {
+      // place people
+      for (ll i = 0; i < B && N>0; ++i) {
+         if (q[i] == 0) {
+            q[i] = m[i];
+            --N;
+            lastI = i;
+         }
+      }
+
+      // make haircut
+      newMinT = 100000;
+      for (int  i = 0; i < B; ++i) {
+         q[i] = q[i] - minT;
+         newMinT = min(newMinT, q[i]);
+      }
+      minT = newMinT;
+   }
+   printf("%d\n", lastI+1);
 }
 
 int main()
 {
-   freopen("A-small-practice.in", "r", stdin);
-   freopen("A-small-practice.out", "w", stdout);
+   freopen("input.txt", "r", stdin);
+   freopen("output.txt", "w", stdout);
 
    int tests;
    scanf("%d", &tests);
